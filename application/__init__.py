d6e1f628507734d83b42c36134f6df1b7fe166a9
Iida Hämäläinen <iida.t.hamalainen@gmail.com>
2019-09-23 11:37:15 +0300

    korjaus postgresql lisäykseen

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,13 @@ app.secret_key = "lets cook"
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -17,6 +22,7 @@ from application.recipes import views
 from application.auth import models
 from application.auth import views
 
+#Kirjautuminen
 from application.auth.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -32,4 +38,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
